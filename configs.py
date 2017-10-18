64acaab495b24090c57166d30a05e844dc927754
Imanity <imanity14@126.com>
2017-10-18 16:56:43 +0800

    Partition

--- configs.py
+++ configs.py
@@ -1,3 +1,6 @@
 chunk = 1024
 channels = 1
 rate = 16000
+min_volume = 200
+min_continuous_time = 1600
+blank_time_gap = 500    # 500ms
--- partition.py
+++ partition.py
@@ -1,14 +1,43 @@
+import pyaudio
 import wave
 import configs
 import scipy.io.wavfile
 import numpy as np
 import matplotlib.pyplot as plt 
 
-def partition(filename):
-    sample_rate, data = scipy.io.wavfile.read(filename)
+def partition(in_filename, output_filename):
+    (sample_rate, data) = scipy.io.wavfile.read(in_filename)
+    
+    begin = -1
+    blank_time = 0
+    partitions = []
+
+    for i in range(0, len(data)):
+        if data[i] > configs.min_volume:
+            blank_time = 0
+            if begin < 0:
+                begin = i
+        else:
+            blank_time += 1
+        
+        if blank_time >= configs.blank_time_gap and begin > 0:
+            end = i - configs.blank_time_gap
+            if end - begin > configs.min_continuous_time:
+                partitions.append((begin, end))
+            begin = -1
+    
+    '''
+    print(partitions)
+
     plt.subplot(2, 1, 1)
     plt.title("1")
     plt.plot(data)
     plt.show()
-
-partition('output.wav')
+    '''
+    
+    for i in range(0, len(partitions)):
+        filename = output_filename + str(i) + '.wav'
+        scipy.io.wavfile.write(filename, sample_rate, data[(partitions[i][0]):(partitions[i][1])])
+    
+if __name__ == "__main__":
+    partition('dialogue.wav', 'part_')
